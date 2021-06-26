445d26d6a2661ecc1085b44ce445ddb67155a607
超神的冰凉 <2801621420@qq.com>
2021-06-26 11:36:48 +0800

    id表格生成器

---
+++ build_script/idLister.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+idFile = open("./src/items/TechDawn_item.js", "r",encoding="utf-8")
+content = idFile.read()
+idFile.close()
+
+idFile2 = open("./src/items/TechDawn_tool.js", "r", encoding="utf-8")
+content += idFile2.read()
+idFile2.close()
+
+idFile3 = open("./src/items/TechDawn_machine.js", "r", encoding="utf-8")
+content += idFile3.read()
+idFile3.close()
+
+for x in content.split("\n"):
+    if x.startswith("new ItemInfo"):
+        tokens = x.replace("new ItemInfo(","").replace('"',"").split(", ")
+        print("|"+tokens[0]+"|"+tokens[2]+"|"+tokens[3]+"|")
